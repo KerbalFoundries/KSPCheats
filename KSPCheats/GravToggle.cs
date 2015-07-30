6afedc7410ea2849cffe2c2592152e9b50c41024
Lo-FiOrbitalScience <ian.elvar@gmail.com>
2015-07-30 18:13:32 +0100

    Initial Commit

---
+++ KSPCheats/GravToggle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace KSPCheats
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class GravToggle : MonoBehaviour
+    {
+        bool gravCheat;
+        double GeeASL;
+
+        public void Start()
+        {
+            GeeASL = FlightGlobals.ActiveVessel.mainBody.GeeASL;
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.O))
+            {
+                gravCheat = !gravCheat;
+                if (gravCheat)
+                    FlightGlobals.ActiveVessel.mainBody.GeeASL = 0;
+                else
+                    FlightGlobals.ActiveVessel.mainBody.GeeASL = GeeASL;
+            }
+        }
+    }
+}
